431edff355c5f3076328244f7319dc312f910b69
Scott Teresi <teresiscott@gmail.com>
2020-02-26 15:07:09 -0500

    Clean image_data and update TODO

--- load_data/import_image_data.py
+++ load_data/import_image_data.py
@@ -22,3 +22,17 @@ def image_data(file_name):
         file_name,
         sep=',',
         header='infer')
+
+    # Drop the RGB columns
+    image_data = image_data.loc[:, ~image_data.columns.str.contains('RGB')]
+
+    # Drop the X and Y columns
+    image_data = image_data.drop(columns=['X','Y'], axis=1)
+
+    # Drop rows with plot_id containing the word fill
+    image_data = image_data[~image_data.plot_id.str.contains('fill')]
+
+    # Drop the last two sequences of numbers from the plot_id column
+    image_data.plot_id = image_data.plot_id.str[:9]  # TODO Confirm with robert
+    # one more time that we can indeed cut the first 9 out and we won't mess up
+    # any names.
