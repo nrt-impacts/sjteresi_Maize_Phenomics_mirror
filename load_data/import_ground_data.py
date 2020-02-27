730b680c0557dbe3dfba38d6629ebe2dfff6ae7d
oceaquaris <oceaquarisoxyheloris@gmail.com>
2020-02-27 16:22:29 -0500

    fix NaN ground data import

--- load_data/import_ground_data.py
+++ load_data/import_ground_data.py
@@ -5,22 +5,40 @@ __author__ = "Anna Haber"
 import pandas as pd
 
 
-def ground_data(file_name):
+def ground_data(file_name, sortcol = "plot"):
     """ Import the ground_data_2019.csv data
 
     Args:
         path_to_file (str)
 
     """
-
+    # read data into pandas.DataFrame
     ground_data = pd.read_csv(
-            file_name,
-            sep=',',
-            header='infer')
+        file_name,
+        sep=',',
+        header='infer'
+    )
 
     # Keep the plot, PlantHeightP1, and PlantHeightP2 columns only.
     ground_data = ground_data[['plot', 'PlantHeightP1', 'PlantHeightP2']]
     ground_data['MeanPHeight'] = ground_data[['PlantHeightP1',
                                               'PlantHeightP2']].mean(axis=1)
 
+    # remove NaN values
+    ground_data = ground_data.loc[~ground_data['MeanPHeight'].isna(),:]
+
+    # sort data by plot_id
+    ground_data = ground_data.sort_values(by=sortcol)
+
     return ground_data
+
+def extract_canopy_ht(df, colname = "MeanPHeight"):
+    """
+    extract canopy heights
+    Assumes a pandas.DataFrame sorted by "plot"
+    """
+    # extract numpy.ndarray copy
+    canopy_ht = df.loc[:,colname].values.copy()
+
+    # return numpy.ndarray
+    return canopy_ht
