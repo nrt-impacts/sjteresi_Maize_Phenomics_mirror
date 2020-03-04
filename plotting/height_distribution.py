c50c0b14be8b5e4c937e6d4c1d0ee265d72516c9
oceaquaris <oceaquarisoxyheloris@gmail.com>
2020-03-04 14:27:16 -0500

    distribution stuff

---
+++ plotting/height_distribution.py
@@ -0,0 +1,8 @@
+from matplotlib import pyplot
+
+def hist_height(arr, imgname):
+    pyplot.hist(arr)
+    pyplot.xlabel("Height (ft above sea level)")
+    pyplot.ylabel("Frequency")
+    pyplot.savefig(imgname)
+    pyplot.clf()
