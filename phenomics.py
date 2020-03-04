fa6274406878bc1d8f685cd87759c347963d3c67
oceaquaris <oceaquarisoxyheloris@gmail.com>
2020-03-04 14:26:54 -0500

    add plots

--- phenomics.py
+++ phenomics.py
@@ -8,9 +8,17 @@ import logging
 import coloredlogs
 from configparser import ConfigParser
 
+from pyswarms.utils.plotters import plot_cost_history
+from matplotlib import pyplot
+from pyswarms.utils.plotters.formatters import Mesher
+from pyswarms.utils.plotters import plot_surface
+
+
 from load_data.import_image_data import image_data, extract_dsm
 from load_data.import_ground_data import ground_data, extract_canopy_ht
 from height_correlation.quantile_optimize import quantile_optimize
+from height_correlation.objective_function import htcor_objfn
+from plotting.height_distribution import hist_height
 
 if __name__ == '__main__':
     # TODO implement a main description
@@ -51,13 +59,18 @@ if __name__ == '__main__':
         groupcol = "plot_id",
         grouprow = HumanData["plot"].values
     )
+
+    hist_height(soil, "soil_hist.png")
+
     # extract data from columns for canopy
     canopy, canopy_size = extract_dsm(
         DroneData,
-        colname = "DSM_10_7_19", # this is the last date (caution: dead plants?)
+        colname = "DSM_9_2_19", # this is the last date (caution: dead plants?)
         groupcol = "plot_id",
         grouprow = HumanData["plot"].values
     )
+
+    hist_height(canopy, "canopy_hist.png")
 
     # extract canopy manually taken heights from pandas.DataFrame
     manual_ht = extract_canopy_ht(
@@ -65,7 +78,7 @@ if __name__ == '__main__':
     )
 
     # identify optimal quantile settings
-    cost, pos = quantile_optimize(
+    cost, pos, optimizer = quantile_optimize(
         z_soil = soil,
         z_soil_size = soil_size,
         z_canopy = canopy,
@@ -78,3 +91,6 @@ if __name__ == '__main__':
     print("Objfn cost:", cost)
     print("Soil quantile:", pos[0])
     print("Canopy quantile:", pos[1])
+
+    plot_cost_history(optimizer.cost_history)
+    pyplot.savefig("pso_cost.png")
