f1f438df00f7279005c42ffe3d2b471aa3b7aaab
achaber <haberan2@msu.edu>
2020-03-18 15:21:49 -0400

    Set default command line arg and load in data files

--- random_forest/random_forest.py
+++ random_forest/random_forest.py
@@ -4,6 +4,8 @@
 Create a random forest to predict robot height values from hand-measured height
 values.
 """
+# Scott, I thought we agreed it was the other way around?
+# (predicting hand-measured height from robot height)
 
 __author__ = "Scott Teresi, Anna Haber"
 
@@ -13,39 +15,42 @@ import argparse
 import logging
 import coloredlogs
 
-# TODO Anna add the import commands for the functions from the other files, I
-# think the .. notation is for going back two folders. Maybe think about this.
-# Can be tricky, my ViM automatically tells me where I am when doing these .
-# notations, so be careful.
-from ..load_data.import_obs_data
+# TODO Scott, do we need the extract_dsm and extract_canopy_ht
+# functions that Robert wrote? I'd think we would if we want to
+# run our machine learning on height values.
+from ..load_data.import_obs_data import obs_data, geno_plot_dict
+from ..load_data.import_image_data import image_data, extract_dsm
+from ..load_data.import_ground_data import ground_data, extract_canopy_ht
+from ..load_data.replace_names import replace_names
 
 
 def validate_args(args, logger):
-    """Raise if an input argument in invalid."""
+    """Raise if an input argument is invalid."""
 
     if not os.path.isfile(args.human_data):
         logger.critical("Argument 'human_data' is not file")
         raise ValueError("%s is not a directory" % (args.human_data))
     if not os.path.isfile(args.drone_data):
         logger.critical("Argument 'drone_data' is not file")
-        raise ValueError("%s is not a directory" % (args.drone_data))
-    # TODO Anna add the check for the obs data, mimic the structure as above
+        raise ValueError("%s is not a directory" % (args.drone_data)) 
+    if not os.path.isfile(args.obs_data):
+        logger.critical("Argument 'obs_data' is not file")
+        raise ValueError("%s is not a directory" % (args.obs_data))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Load input data')
     path_main = os.path.abspath(__file__)
-    # TODO Add default command line argument to the python file for the data
-    # sets, look up the bottom of my code in TE Density density.py if you are
-    # truly stuck. Since this default parameter will probably require a common
-    # path, we should tell everyone in the lab to make a common path, maybe
-    # store the data in a /data folder in the main repository folder.
+
     parser.add_argument('human_data', type=str, help='parent path of human
                         measured height data file')
     parser.add_argument('drone_data', type=str, help='parent path of drone
                         measured height data file')
     parser.add_argument('obs_data', type=str, help='parent path of observation
                         key file')
+    parser.add_argument('--path_to_data', '-p', type=str, default=os.path.join(
+                        path_main, '..', 'data'), help='path to data files within 
+                        git repository directory')
     args = parser.parse_args()
     args.human_data = os.path.abspath(args.human_data)
     args.drone_data = os.path.abspath(args.drone_data)
@@ -60,3 +65,27 @@ if __name__ == '__main__':
     validate_args(args, logger)
 
     logger.info("Ready to import")
+
+    # Importing the data
+    human_data = parser.get('human_data')
+    drone_data = parser.get('drone_data')
+    obs_key = parser.get('obs_data')
+    logger.info('Config file objects have been set...')
+
+    logger.info('Load and clean the drone data...')
+    drone_data = image_data(drone_data)
+
+    logger.info('Load and clean the ground truth data...')
+    human_data = ground_data(human_data)
+
+    logger.info('Load the observation key...')
+    obs_key = obs_data(obs_key)
+
+    logger.info('Constructing dictionary of genotype vs. plot ID...')
+    GenoPlotDict = geno_plot_dict(obs_key)
+
+    logger.info('Replace plot ID with genotype for drone data...')
+    DroneData_Replaced = replace_names(drone_data, GenoPlotDict, 'plot_id')
+
+    logger.info('Replace plot ID with genotype for ground truth data...')
+    HumanData_Replaced = replace_names(human_data, GenoPlotDict, 'plot')
