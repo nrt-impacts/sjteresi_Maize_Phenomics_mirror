348353a10bb2f8a6714c38cf198937b0084d6b63
oceaquaris <rzshrote@gmail.com>
2020-03-19 14:00:21 -0400

    add read data

--- logistic_model/nls.R
+++ logistic_model/nls.R
@@ -1,23 +1,16 @@
-<<<<<<< HEAD
 # Ruthlessly stolen from:
 # https://stats.stackexchange.com/questions/47802/whats-the-most-pain-free-way-to-fit-logistic-growth-curves-in-r
 
-library("nls")
+df <- read.csv("../quantiles.csv", header=T)
+
+y <- df$manual_ht
+x1 <- df$data_07_02.0.21092202170754804
+x2 <- df$data_09_02.0.9946622335380833
+
+htlm <- lm(y ~ x1 + x2)
 
 # subset data
 DNase1 <- subset(DNase, Run == 1)
 
 # using a selfStart model
 fm1DNase1 <- nls(density ~ SSlogis(log(conc), Asym, xmid, scal), DNase1)
-=======
-# Ruthlessly stolen from:
-# https://stats.stackexchange.com/questions/47802/whats-the-most-pain-free-way-to-fit-logistic-growth-curves-in-r
-
-library("nls")
-
-# subset data
-DNase1 <- subset(DNase, Run == 1)
-
-# using a selfStart model
-fm1DNase1 <- nls(density ~ SSlogis(log(conc), Asym, xmid, scal), DNase1)
->>>>>>> 458063e80116965c7a0c8e0ef958bd3b2d8ba35e
